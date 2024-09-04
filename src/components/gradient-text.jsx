7fd820dbc1f49dbbaf39bbca0f34096c3049a937
djubx <dheeraj.jha.ub@gmail.com>
2024-09-05 01:07:26 +0530

    Fix shikha issue

--- src/components/gradient-text.jsx
+++ src/components/gradient-text.jsx
@@ -3,13 +3,9 @@ import React from "react";
 
 const GradientText = ({ className, text = "" }) => {
   return (
-    <span
-      className={cn(
-        className,
-        "bg-gradient-to-r from-[#16509B] to-[#62A5DA] bg-clip-text text-transparent"
-      )}
-    >
-      {text}
+    <span className={cn(className, "relative inline-block")}>
+      <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#16509B] to-[#62A5DA] opacity-75 mix-blend-overlay" />
+      <span className="relative z-10">{text}</span>
     </span>
   );
 };
