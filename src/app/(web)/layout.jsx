9dcb3c180ad3bffa700deca6296887a3cf9c479a
Dheeraj Jha <iamdheerajjha@gmail.com>
2024-11-04 21:08:41 +0000

    Setup datafa.st

--- src/app/(web)/layout.jsx
+++ src/app/(web)/layout.jsx
@@ -1,8 +1,17 @@
 import NextAuthProvider from "@/components/AuthProvider/AuthProvider";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <div>
+      <Head>
+        <script
+          defer
+          data-website-id="672936034a567f1d37c69ca5"
+          data-domain="realflutter.com"
+          src="https://datafa.st/js/script.js"
+        ></script>
+      </Head>
       <NextAuthProvider>{children}</NextAuthProvider>
     </div>
   );
